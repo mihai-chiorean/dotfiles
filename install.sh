c4fa805d7cec8ca99a9247eb9c44d4679fa1e266
Mihai Chiorean <mihai@uber.com>
2016-03-16 14:24:46 -0700

    feat: spotify from terminal/vim

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ if [ "$(uname)" == "Darwin" ]; then
     /usr/local/bin/brew update
     /usr/local/bin/brew upgrade `cat packages.txt`
     /usr/local/bin/brew install --dry-run `cat packages.txt`
+
+    cd $HOME/bin && wget https://raw.githubusercontent.com/hnarayanan/shpotify/master/spotify
+    chmod +x spotify
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # Do something under GNU/Linux platformi
     echo 'install.sh: linux'
---
+++ vim/.vim/modules/plugins/spotify.vim
@@ -0,0 +1,2 @@
+nnoremap <leader>sn :silent :! spotify next<CR> :redraw!<CR>
+nnoremap <leader>sb :silent :! spotify prev<CR> :redraw!<CR>
