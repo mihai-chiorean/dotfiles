f5efcc143c5ec0344054a4fc322e2748cef4008d
Mihai Chiorean <mihai@knotch.it>
2016-03-17 21:47:41 -0700

    feat: addapt to /Users/mihaichiorean

--- setup.sh
+++ setup.sh
@@ -4,5 +4,9 @@ if [ ! -f $HOME/Library/LaunchAgents ]; then
     mkdir -p $HOME/Library/LaunchAgents
 fi
 
+
 cp local.job.plist $HOME/Library/LaunchAgents
+
+home="\"${HOME}\""
+sed -i -- "s_\$HOME_${HOME}_g" $HOME/Library/LaunchAgents/local.job.plist
 launchctl start "local.dotfiles"
